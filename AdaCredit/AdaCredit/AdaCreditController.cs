748d54624b5d364f89be2201b43f082b54dde446
Arthur Prado De Fazio <Arthur_Prado_De_Fazio@MacBook-Pro-Arthur-Prado-De-Fazio.local>
2023-01-02 14:10:32 -0300

    Adiciona inicialização de menus

--- AdaCredit/AdaCredit/AdaCreditController.cs
+++ AdaCredit/AdaCredit/AdaCreditController.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 namespace AdaCredit
 {
-	public class AdaCreditController
+	public partial class AdaCreditController
 	{
 		public void RunApplication(string[] args)
 		{
-			// mostra login
-			// loop que pega input, processa
-			// depois de processar decide -> ou chama uma outra view, ou chama alguma operacao do modelo
-			// se chamou operacao do modelo, possivelmente exibe alguma mensagem e volta pra tela inicial
-			var view = new AdaCredit.View();
-			_ = view.HomeMenu(args);
+			InitMenus(args);
 		}
-	}
+
+        
+    }
 }
 
---
+++ AdaCredit/AdaCredit/EmptyClass.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using ConsoleTools;
+
+namespace AdaCredit
+{
+	public partial class AdaCreditController
+	{
+        private EstadoDeMenu? menuAtual;
+        private EstadoDeMenu? telaLogin;
+        private EstadoDeMenu? areaDoCliente;
+        private EstadoDeMenu? areaDoFuncionario;
+        private EstadoDeMenu? areaDeRelatorios;
+
+        
+        private void InitMenus(string[] args)
+        {
+            InitTelaLogin(args);
+            areaDeRelatorios = new EstadoDeMenu(new ConsoleMenu(args, 2)
+                                                        .Add("Exibir clientes ativos e saldos", () => { })
+                                                        .Add("Exibir clientes inativos", () => { })
+                                                        .Add("Desativar cadastro de funcionário", () => { })
+                                                        .Configure(config =>
+                                                        {
+                                                            config.Selector = ">> ";
+                                                            config.EnableFilter = true;
+                                                            config.Title = "Tela principal";
+                                                            config.EnableBreadcrumb = true;
+                                                            config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
+                                                        }));
+        }
+
+        private void InitTelaLogin(string[] args)
+        {
+            telaLogin = new EstadoDeMenu(new ConsoleMenu(args, 0)
+                                                            .Add("Entre com qualquer tecla para fazer login: ", () => { })
+                                                            .Configure(config =>
+                                                            {
+                                                                config.Selector = ">> ";
+                                                                config.EnableFilter = true;
+                                                                config.Title = "Login";
+                                                                config.EnableBreadcrumb = true;
+                                                                config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
+                                                            }));
+            
+            telaLogin.AdicionaEntrada("Entre com qualquer tecla para fazer login: ", InitTelaPrincipal(args));
+        }
+
+        private static EstadoDeMenu InitTelaPrincipal(string[] args)
+        {
+            EstadoDeMenu telaPrincipal = new(new ConsoleMenu(args, 1)
+                                                    .Add("Área do cliente", () => { })
+                                                    .Add("Área do funcionário", () => { })
+                                                    .Add("Processar de transações", () => { })
+                                                    .Add("Gerar relatório", () => { })
+                                                    .Configure(config =>
+                                                    {
+                                                        config.Selector = ">> ";
+                                                        config.EnableFilter = true;
+                                                        config.Title = "Tela principal";
+                                                        config.EnableBreadcrumb = true;
+                                                        config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
+                                                    }));
+
+            telaPrincipal.AdicionaEntrada("Área do cliente", InitAreaDoClinte(args));
+            telaPrincipal.AdicionaEntrada("Área do funcionário", InitAreaDoFuncionario(args));
+            telaPrincipal.AdicionaEntrada("Processar de transições", InitAreaDeRelatorios(args));
+            telaPrincipal.AdicionaEntrada("Gerar relatório de transições", InitAreaDeRelatorios(args));
+
+            return telaPrincipal;
+        }
+
+
+        private static EstadoDeMenu InitAreaDoClinte(string[] args)
+        {
+            return new(new ConsoleMenu(args, 2)
+                            .Add("Cadastrar novo cliente", () => { })
+                            .Add("Consultar dados de cliente", () => { })
+                            .Add("Alterar cadastro de cliente", () => { })
+                            .Add("Desativar cadastro de cliente", () => { })
+                            .Configure(config =>
+                            {
+                                config.Selector = ">> ";
+                                config.EnableFilter = true;
+                                config.Title = "Tela principal";
+                                config.EnableBreadcrumb = true;
+                                config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
+                            }));
+        }
+
+        private static EstadoDeMenu InitAreaDoFuncionario(string[] args)
+        {
+            return new EstadoDeMenu(new ConsoleMenu(args, 2)
+                                            .Add("Cadastrar novo funcionáro", () => { })
+                                            .Add("Alterar senha", () => { })
+                                            .Add("Desativar cadastro de funcionário", () => { })
+                                            .Configure(config =>
+                                            {
+                                                config.Selector = ">> ";
+                                                config.EnableFilter = true;
+                                                config.Title = "Tela principal";
+                                                config.EnableBreadcrumb = true;
+                                                config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
+                                            }));
+        }
+
+        private static EstadoDeMenu InitAreaDeRelatorios(string[] args)
+        {
+            return new EstadoDeMenu(new ConsoleMenu(args, 2)
+                                                        .Add("Exibir clientes ativos e saldos", () => { })
+                                                        .Add("Exibir clientes inativos", () => { })
+                                                        .Add("Desativar cadastro de funcionário", () => { })
+                                                        .Configure(config =>
+                                                        {
+                                                            config.Selector = ">> ";
+                                                            config.EnableFilter = true;
+                                                            config.Title = "Tela principal";
+                                                            config.EnableBreadcrumb = true;
+                                                            config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
+                                                        }));
+        }
+    }
+}
+
